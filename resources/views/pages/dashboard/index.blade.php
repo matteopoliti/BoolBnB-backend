dd48dc7929b81bd7d3b540340e6adcd3c8a85fcd
matteopoliti <matteo.politi17@gmail.com>
2024-04-26 17:55:46 +0200

    Fix: fixed layou app and added logo

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- @dd($apartments) --}}
+    {{-- @dd($apartments) --}}
     <div class="container-fluid mt-3">
-        <h1>Appartamenti - {{ count($apartments) }}</h1>
-    
+        <h1 class="text-center">Appartamenti - {{ count($apartments) }}</h1>
+
         <div class="mx-3 mt-3">
-            <a href="{{ route('dashboard.apartments.create') }}" class="btn btn-primary mx-auto">
+            <a href="{{ route('dashboard.apartments.create') }}" class="btn btn-primary mx-auto d-block w-25">
                 <i class="bi bi-pencil"></i> Create
             </a>
-    
+
             <table class="table table-striped text-center">
                 <thead>
                     <tr>
@@ -22,7 +22,8 @@
                     @foreach ($apartments as $apartment)
                         <tr>
                             <td>
-                                <a href="{{ route('dashboard.apartments.show', $apartment->slug) }}">{{ $apartment->title }}</a>
+                                <a
+                                    href="{{ route('dashboard.apartments.show', $apartment->slug) }}">{{ $apartment->title }}</a>
                             </td>
                             <td>{{ $apartment->description }}</td>
                             <td>{{ $apartment->created_at }}</td>
@@ -30,39 +31,49 @@
                             <td>
                                 <div class="row">
                                     <div class="col-6">
-                                        <a class="btn btn-warning" href="{{ route('dashboard.apartments.edit', $apartment->slug) }}">
+                                        <a class="btn btn-warning"
+                                            href="{{ route('dashboard.apartments.edit', $apartment->slug) }}">
                                             <i class="fa-solid fa-pen"></i> Modifica
                                         </a>
                                     </div>
                                     <div class="col-6">
-                                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $apartment->id }}">
+                                        <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                            data-bs-target="#deleteModal{{ $apartment->id }}">
                                             <i class="fa-solid fa-trash"></i> Elimina
                                         </button>
                                     </div>
-                                      <!-- Modal -->
-                                    <div class="modal fade" id="deleteModal{{ $apartment->id }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h1 class="modal-title fs-5 text-uppercase" id="staticBackdropLabel">Conferma eliminazione</h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="deleteModal{{ $apartment->id }}" data-bs-backdrop="static"
+                                        data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel"
+                                        aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5 text-uppercase" id="staticBackdropLabel">
+                                                        Conferma eliminazione</h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <strong>Questa operazione è irreversibile.</strong><br>Sei sicuro di
+                                                    voler eliminare l'appartamento: "{{ $apartment->title }}"
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">Annulla</button>
+                                                    <form
+                                                        action="{{ route('dashboard.apartments.destroy', $apartment->slug) }}"
+                                                        method="POST">
+
+                                                        @csrf
+
+                                                        @method('DELETE')
+
+                                                        <input class="btn btn-danger" type="submit" value="Confermo">
+                                                    </form>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div class="modal-body">
-                                            <strong>Questa operazione è irreversibile.</strong><br>Sei sicuro di voler eliminare l'appartamento: "{{ $apartment->title }}"
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                                            <form action="{{ route('dashboard.apartments.destroy', $apartment->slug) }}" method="POST">
-    
-                                            @csrf
-            
-                                            @method('DELETE')
-        
-                                            <input class="btn btn-danger" type="submit" value="Confermo">
-                                        </form>
-                                        </div>
-                                        </div>
-                                    </div>
                                     </div>
                                 </div>
                             </td>
