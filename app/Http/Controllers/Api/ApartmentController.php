238cc73088966c4ace9541be07563b8b4dcabb32
michelearcopinto <guestphantom1@gmail.com>
2024-04-29 20:24:05 +0200

    feat: added availability filter to show page

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -86,7 +86,7 @@ class ApartmentController extends Controller
 
     public function show($slug)
     {
-        $apartment = Apartment::with('services')->where('slug', $slug)->first();
+        $apartment = Apartment::with('services')->where('is_available', 1)->where('slug', $slug)->first();
 
 
         if ($apartment) {
