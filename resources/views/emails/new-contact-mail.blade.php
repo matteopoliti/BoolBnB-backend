60d6ee33369fe2bac89e936ce5d3e5e94fbff345
matteopoliti <matteo.politi17@gmail.com>
2024-04-30 16:35:36 +0200

    Fix: fixed html of new contact mail

--- resources/views/emails/new-contact-mail.blade.php
+++ resources/views/emails/new-contact-mail.blade.php
@@ -1,12 +1,8 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Hai ricevuto un nuovo messaggio:</h1>
-
-    <p>
-        Nome: {{ $message->name }} <br>
-        Email: {{ $message->email }} <br>
-        Message: <br>
-        {{ $message->message }}
-    </p>
-@endsection
+<h1>Ciao Host</h1>
+<h4>Hai ricevuto un nuovo messaggio:</h4>
+<p>
+    Nome: {{ $message->name }} <br>
+    Email: {{ $message->email }} <br>
+    Message: <br>
+    {{ $message->message }}
+</p>
