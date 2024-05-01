fc2b8715978bb1a6b59ef17fa25594e06652d1f0
michelearcopinto <guestphantom1@gmail.com>
2024-05-01 10:28:28 +0200

    fix: adjusted layout welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.access')
 @section('content')
-    <div class="px-4 pt-5 text-center border-bottom bg-light">
+    <div class="px-4 pt-5 text-center border-bottom bg-light" style="height: calc(100vh - 80px)">
         <h1 class="display-4 fw-bold text-body-emphasis">Benvenuto nell'area personale di Boolbnb</h1>
         <div class="col-lg-6 mx-auto">
             <p class="lead mb-4">Carica le informazioni della tua proprietà, imposta il prezzo e connettiti con
@@ -14,7 +14,7 @@
                     @if (Route::has('register'))
                         <div class="">
                             <a class="btn btn-outline-secondary btn-lg px-4"
-                                href="{{ route('register') }}">{{ __('Register') }}</a>
+                                href="{{ route('register') }}">{{ __('Registrati') }}</a>
                         </div>
                     @endif
                 @else
@@ -33,7 +33,7 @@
                 @endguest
             </div>
         </div>
-        <div class="overflow-hidden" style="max-height: 30vh;">
+        <div>
             <div class="container px-5">
                 <img src="{{ Vite::asset('resources/assets/img/Screenshot 2024-04-27 114738.png') }}"
                     class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500"
