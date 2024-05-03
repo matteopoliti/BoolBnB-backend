41edb332ed3eacbe256164d718fba6e43dd9aed8
Anna <annadalex@icloud.com>
2024-05-03 12:13:19 +0200

    Feat:page not-found

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Page Not Found</div>
+
+                    <div class="card-body">
+                        The page you are looking for could not be found.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
