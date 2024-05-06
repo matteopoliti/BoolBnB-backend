1af6c29d6ef6f1750a54a96e3a380ef1b9ed2095
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-05-06 15:43:02 +0200

    feat: message page style

--- resources/views/pages/dashboard/messages.blade.php
+++ resources/views/pages/dashboard/messages.blade.php
@@ -5,12 +5,12 @@
         <h1 class="mb-4 fw-bold">Messaggi ({{ $totalMessages }})</h1>
         <div id="accordion">
             @foreach ($apartments as $apartment)
-                <div class="card mb-4">
-                    <div class="card-header" id="heading{{ $apartment->id }}">
+                <div class="card mb-4 message-card">
+                    <div class="card-header message-card-header" id="heading{{ $apartment->id }}">
                         <h2 class="mb-0">
-                            <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $apartment->id }}" aria-expanded="true" aria-controls="collapse{{ $apartment->id }}">
+                            <button class="btn w-100 text-start" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $apartment->id }}" aria-expanded="true" aria-controls="collapse{{ $apartment->id }}">
                                 @if (Str::startsWith($apartment->cover_image, 'https'))
-                                    <img src="{{ $apartment->cover_image }}" alt="{{ $apartment->slug }}" class="img-thumbnail me-2" style="width: 100px; height: auto;">
+                                    <img src="{{ $apartment->cover_image }}" alt="{{ $apartment->slug }}" class="img-thumbnail me-2 col-2" style="width: 150px; height:auto; max-height: 100px; object-fit:cover;">
                                 @else
                                     <img src="{{ asset('storage/' . $apartment->cover_image) }}" alt="{{ $apartment->slug }}" class="img-thumbnail me-2" style="width: 100px; height: auto;">
                                 @endif
@@ -19,15 +19,15 @@
                         </h2>
                     </div>
                     <div id="collapse{{ $apartment->id }}" class="collapse" aria-labelledby="heading{{ $apartment->id }}" data-bs-parent="#accordion">
-                        <div class="card-body">
+                        <div class="card-body message-card-body">
                             @if ($apartment->messages->isEmpty())
                                 <p class="text-muted">Nessun messaggio per questo appartamento.</p>
                             @else
-                                <ul class="list-group list-group-flush">
+                                <ul class="list-group list-group-flush gap-3">
                                     @foreach ($apartment->messages as $message)
-                                        <li class="list-group-item">
-                                            <p>{{ $message->message }}</p>
-                                            <footer class="blockquote-footer">
+                                        <li class="list-group-item rounded">
+                                            <p class="mb-4">{{ $message->message }}</p>
+                                            <footer class="blockquote-footer mb-1">
                                                 Inviato da <cite>{{ $message->name }}</cite> il <span class="datetime">{{ $message->created_at->toIso8601String() }}</span> |
                                                 <a href="mailto:{{ $message->email }}?subject=Risposta al tuo messaggio per {{ $apartment->title }}&body=Ciao {{ $message->name }},%0D%0A%0D%0AGrazie per il tuo interesse.">
                                                     Rispondi
@@ -46,6 +46,7 @@
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
+
             const dates = document.querySelectorAll('.datetime');
             dates.forEach(function(dateElement) {
                 const utcDate = new Date(dateElement.textContent);
@@ -57,6 +58,27 @@
                     hour: '2-digit',
                     minute: '2-digit'
                 });
+            });
+
+            const accordion = document.getElementById('accordion');
+
+            // Aggiungi event listeners per gli eventi di show e hide del collapse di Bootstrap
+            accordion.addEventListener('show.bs.collapse', function(event) {
+                // Trova il card-header associato al collapse aperto
+                const cardHeader = event.target.previousElementSibling;
+                cardHeader.classList.add('active');
+            });
+
+            accordion.addEventListener('hide.bs.collapse', function(event) {
+                // Trova il card-header associato al collapse che si sta chiudendo
+                const cardHeader = event.target.previousElementSibling;
+                cardHeader.classList.remove('active');
+            });
+
+            // Controlla se qualche collapse è già aperto al caricamento e aggiungi la classe active
+            document.querySelectorAll('.collapse.show').forEach(function(collapseElement) {
+                const cardHeader = collapseElement.previousElementSibling;
+                cardHeader.classList.add('active');
             });
         });
     </script>
